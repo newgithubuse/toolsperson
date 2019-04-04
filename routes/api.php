cf3554790db47440172d07a5d9b4130cf449e500
newgithubuse <peterh320267@gmail.com>
2019-04-04 17:09:43 +0800

    新增v1路由

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,5 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::group(['prefix' => 'user'], function(){
-    Route::post('registered', 'UserController@registered');
-    Route::post('login', 'UserController@login');    
-    Route::post('submit', 'UserController@submitObject');
-});
+
     
