d4adff33c2341eaa5cbe43169779faed0c05bbad
Puroguramu <44017033+Puroguramu@users.noreply.github.com>
2019-04-07 19:22:00 +0800

    修改使用者刊登物件資料表 新增name欄位 (#26)

--- app/Http/v1/Controllers/PublicController.php
+++ app/Http/v1/Controllers/PublicController.php
@@ -3,8 +3,10 @@
 namespace App\Http\v1\Controllers;
 
 use Exception;
+use App\Models\User;
 use App\Models\UserPostEvent;
 use App\Http\v1\Responses\Response;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Request;
 
 class PublicController
