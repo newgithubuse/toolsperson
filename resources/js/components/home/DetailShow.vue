6a620261b13de05a6f5d9b28aa4a320e24a1162d
Puroguramu <44017033+Puroguramu@users.noreply.github.com>
2019-04-07 18:43:18 +0800

    修改刊登內容顯示板面 (#25)

--- resources/js/components/home/DetailShow.vue
+++ resources/js/components/home/DetailShow.vue
@@ -1,40 +1,81 @@
 <template>
-  <div>
-    <div class="alltocenter">
-      <img
-        :src="content.img"
-        alt=""
-      >
+  <div class="border">
+    <h1 class="displaycenter titlestyle">
+      刊登事項
+    </h1>
+    <div class="row mb">
+
+      <div class="col-8">
+        <h2>{{content.title}}</h2>
+      </div>
+
+      <div class="col-4 displayaligncenter">刊登者 :</div>
     </div>
-    <div class="alltocentercolumn">
-      <h1>{{content.title}}</h1>
-      <h2>{{content.text}}</h2>
-      <div style="text-align:center;word-break: break-all">
-        <p>{{content.detial}}</p>
+    <div class="row mb">
+      <div class="col-7 displayaligncenter">
+        <h3>{{content.text}}</h3>
+      </div>
+      <div class="col-5 imgdiv">
+        <img
+          :src="content.img"
+          alt=""
+        >
+      </div>
+    </div>
+    <div class="row mb">
+      <div class="col-12">
+        <h5>
+          {{content.detail}}
+        </h5>
+      </div>
+    </div>
+    <div class="row mb">
+      <div class="col-12 displaycenter">
+        <buttom class="btn btn-primary">報名委託</buttom>
       </div>
     </div>
   </div>
 </template>
-<style>
-  .alltocenter {
+<style lang="scss" scoped>
+  .border {
+    padding: 10px 30px;
+  }
+  .imgdiv {
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .displaycenter {
     display: flex;
     justify-content: center;
   }
-  .alltocentercolumn {
+  .titlestyle {
+    line-height: 80px;
+    box-sizing: border-box;
+    border-bottom: 1px solid #dee2e6;
+  }
+  .displayaligncenter {
     display: flex;
     align-items: center;
-    flex-direction: column;
   }
 </style>
 <script>
   import store from "@/store";
   export default {
+    data() {
+      return {
+        username: ""
+      };
+    },
     computed: {
       content() {
         return this.$store.state.contents.find(item => {
           return item.id == this.$route.params.id;
         });
       }
-    }
+    },
+    methods: {},
+    mounted() {}
   };
 </script>
